61cf157c57838725971961fbabfa37df52822f1f
Nahúm Murillo <85594936+nahum0804@users.noreply.github.com>
2023-10-01 16:23:12 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,44 @@
+//import { readDB } from "./reader.js";
+
+function addCode() {
+    console.log("Se ha llamado a la funcion addCode()");
+    // Llamamos a la funcion que lee la base de datos
+    readDB();
+
+    // Encuentra el elemento con id "add_to_me"
+    var container = document.getElementById("add_to_me");
+
+    // Verifica si ya existe un elemento h3 dentro del contenedor
+    var existingH3 = container.querySelector("#new_h3");
+
+    // Si ya existe un elemento h3, no hacemos nada
+    if (existingH3) {
+        return;
+    }
+
+    // Si no existe un elemento h3, creamos uno y lo agregamos
+    var newH3 = document.createElement("div");
+    newH3.id = "new_h3";
+    //Add dimensions to the div and colors
+    newH3.style.width = "70px";
+    newH3.style.height = "50px";
+    newH3.style.backgroundColor = "blue";
+    newH3.style.borderRadius = "10px";
+    newH3.margin = "auto";
+
+    // Add text to the div
+    container.appendChild(newH3);
+
+    /* Necesitamos agregar 2 imagenes
+    La primera consiste en la grafica de barras de los grados celcius utilizan el promedio por hora
+    para mostrar la temperatura de cada hora del dia
+    
+    La segunda imagen consiste en la grafica de barras de los grados farenheit promedio por hora durante el dia
+    (Lo mismo que la anterior pero en grados ferenheit)
+
+    Idea primordial: Usar D3 para crear las graficas de barras y mostrarlas en la pagina web
+
+    Solo habilitar con min y max los días encontrados en los datos leídos de firebase
+
+    */
+}
---
+++ reader.js
@@ -0,0 +1,21 @@
+import { getFirestore, doc, getDoc } from "firebase/firestore";
+import { initializeApp } from "firebase/app";
+
+
+async function readDB(){
+    const firebaseConf = "C:\Users\Usuario\Desktop\ProyectoDeIoT\artificialTemperatureGenerator\temperaturemonitoring-firebase.json"
+    const app = initializeApp(firebaseConf);
+    const db = getFirestore(app);
+
+    const docRef = doc(db, "dataTemp");
+    const docSnap = await getDoc(docRef);
+
+    if (docSnap.exists()) {
+        console.log("Document data:", docSnap.data());
+      } else {
+        // docSnap.data() will be undefined in this case
+        console.log("No such document!");
+      }
+};
+
+export { readDB };
